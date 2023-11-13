22d977e931ed7f68273585a393840818adf0fe88
Visshall9 <Vishal.cheeti@iiitb.ac.in>
2023-11-13 14:25:09 +0530

    tect commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,47 @@
+import org.example.Main;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+public class CalculatorTest {
+    private Main calculator;
+    @Before
+    public void setUp() {
+        calculator = new Main();
+    }
+    @Test
+    public void test_add() {
+        int a = 10;
+        int aa = 50;
+        int expectedResult = 60;
+        int result = calculator.add(a,aa);
+        Assert.assertEquals(expectedResult, result);
+    }
+
+
+    @Test
+    public void test_subt() {
+        int a = 20;
+        int aa = 5;
+        int expectedResult = 15;
+        int result = calculator.subt(a,aa);
+        Assert.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void test_mul() {
+        int a = 10;
+        int aa = 2
+        int expectedResult = 20;
+        int result = calculator.mul(a,aa);
+        Assert.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void test_per() {
+        int a = 10;
+        int b =4
+        int expectedResult = 2
+        int result = calculator.per(a,b);
+        Assert.assertEquals(expectedResult, result);
+    }
+}
